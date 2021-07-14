456eb36226c6c3b86736d17a1c785d1b684a286b
jaesoek <ljs981120@gmail.com>
2021-07-14 11:03:12 +0900

    "category bug fixed"

--- src/components/StyledLi.jsx
+++ src/components/StyledLi.jsx
@@ -11,24 +11,29 @@ const WrapperDiv = styled.div`
     position:absolute;
     list-style:none;
     // z-index:-1;
-    bottom:-300%;
+    bottom:-500%;
     opacity: 0;
     transition: bottom 0.3s;
   }
 
-  &>ul::before{
-    content:'';
-    display:block;
-    height:2.31rem;
-
-
-  }
-
+  // &>ul::before{
+  //   content:'';
+  //   display:block;
+  //   height:2.31rem;
+  // }
+  
   &:hover > ul{
     // background-color: grey;
     display:block;
-    bottom: -500%;
+    bottom: -700%;
     opacity: 1;
+    &>li:first-child{
+      padding-top:3rem;
+    }
+  }
+
+  &> ul>li{
+    padding:0.5rem 0;
   }
 `
 
